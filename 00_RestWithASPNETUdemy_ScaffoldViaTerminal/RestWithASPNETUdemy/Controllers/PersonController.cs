29be238225bf621e66c748d3962fd99046b12c21
VINICIUS OLIMPIO <vinicius.olimpio@fatec.sp.gov.br>
2021-06-29 20:18:37 -0300

    Working with four verbs

---
+++ 00_RestWithASPNETUdemy_ScaffoldViaTerminal/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+        //Sum é o metodo, first number primeiro parametro e second number segundo parametro
+        [HttpGet("Sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Input Invalido");
+        }
+    }
+}
